0ebd124734271999f0ec38092528e45d6349fb76
Abdulmannan353 <78361642+Abdulmannan353@users.noreply.github.com>
2022-12-23 23:26:29 +0500

    code added
    code added

---
+++ client/build/precache-manifest.57d3ebf0e027a4f023214b233fc84619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51294105b7fbad720479",
+    "url": "/static/css/main.d29e396f.chunk.css"
+  },
+  {
+    "revision": "51294105b7fbad720479",
+    "url": "/static/js/main.51294105.chunk.js"
+  },
+  {
+    "revision": "3080bf60c485e558438b",
+    "url": "/static/js/1.3080bf60.chunk.js"
+  },
+  {
+    "revision": "6037ddbdb55107d6ad72",
+    "url": "/static/css/2.64af919f.chunk.css"
+  },
+  {
+    "revision": "6037ddbdb55107d6ad72",
+    "url": "/static/js/2.6037ddbd.chunk.js"
+  },
+  {
+    "revision": "16662f342644c6f0be82",
+    "url": "/static/js/runtime~main.16662f34.js"
+  },
+  {
+    "revision": "27a6b00421a6916387993892a958423f",
+    "url": "/index.html"
+  }
+];
